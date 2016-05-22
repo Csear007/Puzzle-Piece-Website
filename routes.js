d9e6a4366ebf1887c41b1c8a0e58f58ed74ad6a0
Cesar Salazar <cesarsalazar09@gmail.com>
2016-05-21 22:29:25 -0500

    Modified routes.js to handle all html and css page requests, changed server.js accordingly.

--- routes.js
+++ routes.js
@@ -1,32 +1,62 @@
 var fs = require("fs");
 
 
-function about(request, response){
+function routeHTML(request, response){
    if (request.method == "GET"){
 
-      if(request.url == "/"){
-	response.writeHead( 200, {'Content-type': "text/html"}
-	fs.createReadStream("./views/homepage.html").pipe(response);
-     }
+    if(request.url == "/" || request.url == "/homepage"){
+		response.writeHead( 200, {'Content-type': "text/html"});
+		fs.createReadStream("./views/homepage.html").pipe(response);
+    }
 	else if ( request.url == "/about") {
-        
-          
+		response.writeHead( 200, {'Content-type': "text/html"});
+		fs.createReadStream("./views/about.html").pipe(response);
+	}
+	else if ( request.url == "/puzzle") {
+		response.writeHead( 200, {'Content-type': "text/html"});
+		fs.createReadStream("./views/puzzle-page.html").pipe(response);
+	}
+	else if ( request.url == "/sudoku") {
+		response.writeHead( 200, {'Content-type': "text/html"});
+		fs.createReadStream("./views/sudoku-page.html").pipe(response);
+	}
+	else if ( request.url == "/tic-tac") {
+		response.writeHead( 200, {'Content-type': "text/html"});
+		fs.createReadStream("./views/tic-tac.html").pipe(response);
+	}
+	else
+	{
+		response.writeHead( 404, {'Content-type': "text/plain"});
+		response.write("The page you were searching for was not found\n Sorry!");
+		response.end();
+	}
+  }
+} 
 
+function routeCSS(request, response){
 
+	if( request.url == "/public/stylesheets/homepage.css"){
+		response.writeHead(200, {'Content-type': "text/css"});
+		fs.createReadStream("../public/stylesheets/homepage.css").pipe(response);
+		response.end();
+	}
+	else if ( request.url == "/public/stylesheets/about.css"){
+		response.writeHead(200, {'Content-type': "text/css"});
+		fs.createReadStream("../public/stylesheets/about.css").pipe(response);
+		response.end();
+	}
+	else if ( request.url == "/public/stylesheets/puzzle-page.css"){
+		response.writeHead(200, {'Content-type': "text/css"});
+		fs.createReadStream("../public/stylesheets/puzzle-page.css").pipe(response);
+		response.end();
+	}
+	else 
+	{
+		console.log(request.url);
+	}
+}  
 
+module.exports.routeHTML = routeHTML;
+module.exports.routeCSS = routeCSS;       
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
 	
--- server.js
+++ server.js
@@ -1,41 +1,12 @@
 var http = require('http');
 var fs = require("fs");
 
-
-// 404 response
-function send404Response(response){
-	response.writeHead(404, {"Content-Type": "text/plain"});
-	response.write("Error 404: Page not found!");
-	response.end();
-
-}
-
-
-//handles a user request
-function onRequest(request, response) {
-
-
-	
+var router = require("./routes.js");
 
 
 
-
-
-	if ( request.method == "GET" && request.url == '/' ) {
-		response.writeHead(200, {"Content-Type": "text/html"});
-		fs.createReadStream("./views/homepage.html").pipe(response);
-
-	} else {
-		
-		send404Response(response);
-	
-
-	}
-
-
-}
-
-
-
-http.createServer(onRequest).listen(3000);
+http.createServer(function(request, response) {
+	router.routeHTML(request, response);
+	router.routeCSS(request, response);
+}).listen(3000);
 console.log("server is listening on port 3000");
