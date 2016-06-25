8950aac2e225a0a194c089de33e982d5f9779b5b
Cesar Salazar <cesarsalazar09@gmail.com>
2016-06-25 12:43:49 -0500

    Add error box logic, and color change for button themes

--- public/javascripts/tic.js
+++ public/javascripts/tic.js
@@ -5,6 +5,19 @@ var players = null;
 var color_scheme = null; 
 
 $('#start_button').click( function (){
+
+
+	(players == null || color_scheme == null) ? $('.error_box').show() : $('.error_box').hide();
+
+	// if( players == null || color_scheme == null)
+	// {
+	// 	$('.error_box').show();
+	// }
+	// else {
+	// 	$('.error_box').hide();
+	// }
+
+
 	$(".container .jumbotron").load();
 
 	console.log("start button clicked");
@@ -24,3 +37,30 @@ $('.container .jumbotron .colors .btn-group button').click( function (){
 
 	console.log("colors button clicked");
 });
+
+
+$('.colors .btn-group .btn-success').mouseenter(function(){
+
+		var color = $(this).text();
+
+
+		switch(color){
+			case 'Christmas':
+				$(this).css("color", "red");
+				break;
+			case 'Halloween':
+				$(this).css("background-color", "orange");
+				$(this).css("color", "black");
+				break;
+			case 'Simple-Games':
+				$(this).css("background-color", "#89C4FF");
+				$(this).css("color", "#FF5722");
+				break;
+			case 'Plain':
+				$(this).css("background-color", "white");
+
+		}
+
+	console.log("mouseenter is working" + color);
+});
+
