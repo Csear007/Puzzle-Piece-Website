e5a70bd5104e0c3d58c110deba9ee36d2cf572be
Cesar Salazar <cesarsalazar09@gmail.com>
2016-06-27 23:58:16 -0500

    Create test folder for Project

---
+++ test/test.js
@@ -0,0 +1,28 @@
+// testing script 
+
+
+
+var assert = require("assert");
+
+
+
+
+describe("server should return requests", function (){
+
+	it("should return html requests", function(){
+
+	})
+
+	it("should return css requests", function(){
+
+	})
+
+	it("should return javascript requests", function(){
+
+	})
+
+	it("should return image requests requests", function(){
+
+	})
+
+});
