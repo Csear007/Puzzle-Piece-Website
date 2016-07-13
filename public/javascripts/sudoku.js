522626976a06b67784d5960c5349e8ad96039d1d
Cesar Salazar <cesarsalazar09@gmail.com>
2016-07-13 10:46:19 -0500

    included sudoku-logic as javascript object, got all buttons to work, and included sudoku-grid via ajax

--- public/javascripts/sudoku.js
+++ public/javascripts/sudoku.js
@@ -5,11 +5,18 @@ var timer = null;
 var err_check = null;
 
 
+$.ajax("./public/javascripts/sudoku-logic.js", 
+{
+	method: 'GET',
+	dataType: "script",
+	processData: 'true',
+	cache: 'true',
+	success: function(data, textStatus, jqXHR){
+		sudoku = data;
+	}
 
-$.get("./public/javascripts/sudoku-logic.js", function(data) {
-	sudoku = data;
-}); 
 
+});
 
 $(document).ready( function(){
 	$('.container .jumbotron .stage').load('sudoku-btn.html');
@@ -17,28 +24,66 @@ $(document).ready( function(){
 });
 
 
-$('.btn btn-success').click(function() {
-	difficulty = $('.btn btn-success').val();
-
+$('.stage').on("click", ".center .btn-primary", function() {
+	difficulty = $(this).text();
+	console.log(difficulty);
 });
 
-$('.btn btn-primary').click(function() {
-	difficulty = $('.btn btn-primary').val();
-
+$('.stage').on("click", ".center .btn-success", function() {
+	difficulty = $(this).text();
+	console.log(difficulty);
 });
 
-$('.btn btn-danger').click(function() {
-	difficulty = $('.btn btn-danger').val();
+$('.stage').on("click", ".center .btn-danger", function() {
+	difficulty = $(this).text();
+	console.log(difficulty);
+});
 
+$('.container .jumbotron .stage').on("click", ".center .dropdown .dropdown-menu .timer",function() {
+
+	timer = $(this).text();
+	console.log("timer var equals "+timer);
+});
+
+$('.stage').on("click", ".center .dropdown .dropdown-menu .error",function() {
+
+	err_check = $(this).text();
+	console.log("err_check var equals "+err_check);
 });
 
 
 
-$('#start_button').click(function() {
+$('.stage').on("click", "#start_button", function() {
 
 
-	var puzzle = sudoku.generate(difficulty.toLowerCase());
+	var puzzle = sudoku.generate(difficulty.toLowerCase().trim());
 
-	$('.container .jumbotron .stage').html();
+	$('.container .jumbotron .stage').load("./sudoku-grid.html", function() {
+
+		$.each(puzzle, function(index, value){
+
+		var row = index.substr(0, 1);
+		var column = index.substr(1);
+
+		switch(column){
+			case '1': column = "one"; break;
+			case '2': column = "two"; break;
+			case '3': column = "three"; break;
+			case '4': column = "four"; break;
+			case '5': column = "five"; break;
+			case '6': column = "six"; break;
+			case '7': column = "seven"; break;
+			case '8': column = "eight"; break;
+			case '9': column = "nine"; break;
+		}
+
+		$('#'+row+" ."+column).html(value);
+
+		});
+
+	});
+
+
+
 
 });
