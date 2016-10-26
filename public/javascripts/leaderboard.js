b9a335339f8b0e4d7c6b3d6918778714c5036aaa
Cesar Salazar <cesarsalazar09@gmail.com>
2016-10-26 17:15:06 -0500

    formats data from database to html response

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -1,5 +1,6 @@
 // this is the database script 
 var mysql = require("mysql");
+var query_parser = require("query-string");
 
 
 var client = mysql.createConnection({
@@ -9,11 +10,59 @@ var client = mysql.createConnection({
 	database: "sampleDB"
 });
 
-client.query("SELECT * FROM `users_data`", function(err, rows){
-	if(err){
-		console.log("error in connecting to database");
-	}
+var return_data = "";
 
-	console.log("data received from database, SUCCESS!");
-	console.log(rows);
+
+
+var callback = client.query("Select * From users_data", function(err, data){
+
+		if(err){
+			console.log("error in select query");
+		}
+		else {
+			console.log("success in querying data");
+		}
+
+		var leaderboard_head = `<thead class='heading'>
+							<tr><td>Name</td><td>UserName</td><td>Password</td>
+						</thead>`;
+
+		var leaderboard_body = "<tbody class='data_columns'>";
+
+		for(var row = 0; row < data.length; row++){
+			leaderboard_body += "<tr>";
+			leaderboard_body += "<td>" + data[row].Name + "</td>";
+			leaderboard_body += "<td>" + data[row].Username + "</td>";
+			leaderboard_body += "<td>" + data[row].Password + "</td>";
+			leaderboard_body += "</tr>"
+
+		}
+
+		leaderboard_body += "</tbody>";
+
+
+		return_data = "<table class='leader-grid'>" + leaderboard_head + leaderboard_body + "</table>";
+
+
+
+
+	});
+
+
+
+
+function query(querystring, callback){
+
+	var query_object = query_parser.parse(querystring);
+
+
+
+
+	return return_data;
+
+
+
 }
+
+
+module.exports.querying = query;
